c57661fb156a9a82e8b229e283b06644323412b5
Henrique <148980888+henrique1mp@users.noreply.github.com>
2023-11-09 13:16:19 -0300

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('C:\\Users\\henriest\\FakeStore\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('C:\\Users\\henriest\\FakeStore\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('C:\\Users\\henriest\\FakeStore\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('C:\\Users\\henriest\\FakeStore\\my-website\\my-website\\src\\css\\custom.css'),
-];
